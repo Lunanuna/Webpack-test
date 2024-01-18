a2bb281e322d0a80457960f07958602f91bfc308
Lunanuna <hanga93@gmail.com>
2024-01-18 19:30:35 +0900

    This has some error but will push it anyways

--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
 import _ from 'lodash'
-import myName from './myName';
+import './style.css';
+import Icon from './icon.jpeg';
+import printMe from './print';
 
+if (process.env.NODE_ENV !== 'production') {
+    console.log('Looks like we are in development mode!');
+  }
+  
 function component() {
     const element = document.createElement('div');
+    const btn = document.createElement('button')
 
       // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.textContent = myName('Gayoung');
-  
+    element.classList.add('hello');
+
+    //Add the image to our existing div.
+    const myIcon = new Image();
+    myIcon.src = Icon;
+
+    element.appendChild(myIcon);
+
+    //add button
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+
+    element.appendChild(btn);
+
     return element;
   }
   
